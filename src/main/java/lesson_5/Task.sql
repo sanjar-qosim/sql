61d63639786f51f83f6e7e6d2293b69dfa0679fe
Sanjar <sanjar171766@gmail.com>
2025-11-02 22:02:19 +0500

    hw 5 (6-9)

--- src/main/java/lesson_5/Task.sql
+++ src/main/java/lesson_5/Task.sql
@@ -7,3 +7,11 @@ select distinct * from departments d join employees e on e.department_id = d.id 
 select p.name, sum(oi.quantity) as total from order_items oi join products p on p.id = oi.product_id group by p.name order by total desc
 
 select c.name, count(o.id) as total from customers c left join orders o on o.customer_id = c.id group by c.name
+
+select d.name, avg(e.salary) as avg_salary from departments d join employees e on e.department_id = d.id group by d.name order by avg_salary desc limit 3
+
+select c.* from customers c left join orders o on o.customer_id = c.id where o.id is null
+
+select * from employees e where salary > all (select salary from employees e where position like '%manager%')
+
+select d.name from departments d join employees e on e.department_id = d.id group by d.name having min(e.salary) > 5000
